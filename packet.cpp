f65f820878ddb7567413e5158a7d8abd4e8b935d
hung <hbui2@huskers.unl.edu>
2023-09-19 22:05:19 -0500

    working file with average temp

---
+++ packet.cpp
@@ -0,0 +1,43 @@
+#include "USB/USBAPI.h"
+#include "packet.h"
+
+String packetSetup(Packet p) {
+
+  String type = p.type;
+  String packet_id = p.packetId;
+  String node_id = p.nodeId;
+  String payload = p.payload;
+  String time_stamp = String(p.timeStamp);
+  
+  String message = type + "," + packet_id + "," + node_id + "," + payload + "," + time_stamp;
+
+  return message;
+}
+
+Packet packetParser(String str){
+  int first_comma = str.indexOf(",");
+  int second_comma = str.indexOf(",", first_comma + 1);
+  int third_comma = str.indexOf(",", second_comma + 1);
+  int fourth_comma = str.indexOf(",", third_comma + 1 );
+
+  String type = str.substring(0, first_comma);
+  String packet_id = str.substring(first_comma + 1, second_comma);
+  
+  String node_id = str.substring(second_comma + 1, third_comma);
+  String payload = str.substring(third_comma + 1, fourth_comma);
+  long time_stamp = atol(str.substring(fourth_comma, str.length()).c_str());
+
+  Packet p = {type, packet_id, node_id, payload, time_stamp};
+
+  return p;
+}
+
+void serialPrintPacket(Packet p) {
+  SerialUSB.println("type = " + p.type);
+  SerialUSB.println("packet_id = " + p.packetId);
+  SerialUSB.println("node id = " + p.nodeId);
+  SerialUSB.println("payload = " + p.payload);
+  SerialUSB.print("timeStamp = ");
+  SerialUSB.println(p.timeStamp);
+  SerialUSB.println("--------------");
+}
---
+++ packet.h
@@ -0,0 +1,40 @@
+#ifndef PACKET_H
+#define PACKET_H
+#include <Arduino.h>
+
+enum ErrorCode {
+  NO_ERROR = 0,
+  WDT_RESET = 1,
+  RECEPTION_FAIL = 2
+};
+
+struct Packet {
+  String type;
+  String packetId;
+  String nodeId;
+  String payload;
+  long timeStamp;
+};
+
+struct ErrorPacket {
+  String packetId;
+  int nodeId;
+  ErrorCode errCode;
+  unsigned long timeStamp;
+};
+
+struct TemperaturePacket {
+  String packetId;
+  int nodeId;
+  float temp;
+  unsigned long timeStamp;
+};
+Packet packetParser(String str);
+
+String packetSetup(Packet p);
+
+TemperaturePacket tempPacketParser(String str);
+
+void serialPrintPacket(Packet p);
+
+#endif
